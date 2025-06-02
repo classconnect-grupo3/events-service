9fe0b131edb486f32e4fa4f388ab4e6cbce5c72d
tinchomorilla <mmorilla@fi.uba.ar>
2025-06-02 16:54:57 -0300

    add function to retrieve notification preferences by user ID

---
+++ src/repository/notifications_preferences.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+from src.model.notification_preferences import NotificationPreferences
+
+
+def get_preferences_by_user_id(
+    db: Session, user_id: str
+) -> list[NotificationPreferences]:
+    return (
+        db.query(NotificationPreferences)
+        .filter(NotificationPreferences.user_id == user_id)
+        .all()
+    )
