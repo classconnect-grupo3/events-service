745d96290d5df46d94cd751e7f5401a21181b141
tinchomorilla <mmorilla@fi.uba.ar>
2025-06-04 14:36:03 -0300

    refactor: fetch user data (email)  just if it the student has the email notifications enabled otherwise we not fetch the data.

--- src/handlers/assignment_created.py
+++ src/handlers/assignment_created.py
@@ -42,26 +42,26 @@ async def handle_assignment_created_event(db: Session, event: AssignmentCreatedE
     for enrollment in enrollments:
         student_id = enrollment["student_id"]
 
-        # Get user data to get their email
-        try:
-            async with httpx.AsyncClient() as client:
-                user_response = await client.get(
-                    f"https://users-service-production-968d.up.railway.app/users/{student_id}"
-                )
-                user_response.raise_for_status()
-                user_data = user_response.json()["data"]
-                user_email = user_data["email"]
-        except Exception as e:
-            logger.error(
-                f"No se pudo obtener información del usuario {student_id}: {e}"
-            )
-            continue
-
         preferences = get_preferences_by_user_id(db, student_id)
         pref = next((p for p in preferences if p.event_type == event.event_type), None)
 
         if pref:
             if pref.email_enabled:
+                # Get user data to get their email
+                try:
+                    async with httpx.AsyncClient() as client:
+                        user_response = await client.get(
+                            f"https://users-service-production-968d.up.railway.app/users/{student_id}"
+                        )
+                        user_response.raise_for_status()
+                        user_data = user_response.json()["data"]
+                        user_email = user_data["email"]
+                except Exception as e:
+                    logger.error(
+                        f"No se pudo obtener información del usuario {student_id}: {e}"
+                    )
+                    continue
+
                 logger.info(
                     f"Enviando email a {user_email} sobre la asignación {event.assignment_title}"
                 )
